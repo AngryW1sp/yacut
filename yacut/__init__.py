20094f8c537e9c3a857e3720200a50ec8c827a88
AngryW1sp <147386694+AngryW1sp@users.noreply.github.com>
2025-10-15 19:44:19 +0300

    111111

--- yacut/__init__.py
+++ yacut/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from . import models
 from settings import BaseConfig
 
 app = Flask(__name__)
@@ -10,4 +9,4 @@ app.config.from_object(BaseConfig)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from yacut import api_views, views
+from yacut import api_views, views, models
